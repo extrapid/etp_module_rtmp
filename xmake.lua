970c8fc774eda9200f77e74f1a22440666e91b7e
Bradford Zhang <zyc@zyc.name>
2022-09-23 17:27:41 +0800

    xmake.lua: fix xmake
    Signed-off-by: Bradford Zhang <zyc@zyc.name>

--- xmake.lua
+++ xmake.lua
@@ -1,87 +1,24 @@
 add_rules("mode.debug", "mode.release")
 add_includedirs(".","net","xop","mongoose")
-add_files("net/*.cpp","xop/*.cpp","mongoose/*.c","./*.cpp","./*.c")
 add_cxxflags("-shared -fPIC")
-add_syslinks("dl", "pthread","m")
+add_syslinks("dl", "pthread", "m")
 
-target("etp_module_rtmp")
-set_kind("shared")
+target("deps")
+    set_kind("static")
+    add_files("net/*.cpp")
+    add_files("xop/*.cpp")
+    add_files("mongoose/*.c")
+    on_install(function (target)                         end)                                                 on_uninstall(function (target)                       end)
+
+target("etp_sdk")
     set_kind("static")
     add_files("modulesdk.c")
+    on_install(function (target)                         end)                                                 on_uninstall(function (target)                       end)
 
-target("main")
-set_kind("shared")
-    set_kind("binary")
+target("etp_module_rtmp")
+    set_kind("shared")
+    add_deps("etp_sdk", "deps")
+    set_filename("etp_module_rtmp.so")
     add_files("main.cpp")
 
-    add_deps("etp_module_rtmp")
-
---
--- If you want to known more usage about xmake, please see https://xmake.io
---
--- ## FAQ
---
--- You can enter the project directory firstly before building project.
---
---   $ cd projectdir
---
--- 1. How to build project?
---
---   $ xmake
---
--- 2. How to configure project?
---
---   $ xmake f -p [macosx|linux|iphoneos ..] -a [x86_64|i386|arm64 ..] -m [debug|release]
---
--- 3. Where is the build output directory?
---
---   The default output directory is `./build` and you can configure the output directory.
---
---   $ xmake f -o outputdir
---   $ xmake
---
--- 4. How to run and debug target after building project?
---
---   $ xmake run [targetname]
---   $ xmake run -d [targetname]
---
--- 5. How to install target to the system directory or other output directory?
---
---   $ xmake install
---   $ xmake install -o installdir
---
--- 6. Add some frequently-used compilation flags in xmake.lua
---
--- @code
---    -- add debug and release modes
---    add_rules("mode.debug", "mode.release")
---
---    -- add macro defination
---    add_defines("NDEBUG", "_GNU_SOURCE=1")
---
---    -- set warning all as error
---    set_warnings("all", "error")
---
---    -- set language: c99, c++11
---    set_languages("c99", "c++11")
---
---    -- set optimization: none, faster, fastest, smallest
---    set_optimize("fastest")
---
---    -- add include search directories
---    add_includedirs("/usr/include", "/usr/local/include")
---
---    -- add link libraries and search directories
---    add_links("tbox")
---    add_linkdirs("/usr/local/lib", "/usr/lib")
---
---    -- add system link libraries
---    add_syslinks("z", "pthread")
---
---    -- add compilation and link flags
---    add_cxflags("-stdnolib", "-fno-strict-aliasing")
---    add_ldflags("-L/usr/local/lib", "-lpthread", {force = true})
---
--- @endcode
---
 
