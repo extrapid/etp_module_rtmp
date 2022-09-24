c590dd53b9524b5d6976719a8d355ef2f43acd59
Bradford Zhang <zyc@zyc.name>
2022-09-24 15:34:06 +0800

    fix log.cpp renamed from log.c
    Signed-off-by: Bradford Zhang <zyc@zyc.name>

--- xmake.lua
+++ xmake.lua
@@ -4,19 +4,26 @@ add_cxxflags("-shared -fPIC")
 add_cflags("-shared -fPIC")
 add_syslinks("dl", "pthread", "m")
 
+target("etp_static_log")
+    set_kind("static")
+    add_files("log.cpp")
+    on_install(function (target)end)
+    on_uninstall(function (target)end)
+
 target("deps")
     set_kind("static")
+    add_deps("etp_static_log")
     add_files("net/*.cpp")
     add_files("xop/*.cpp")
     add_files("mongoose/*.c")
-    add_files("./modulesdk.cpp")
-    on_install(function (target)                         end)                                                 on_uninstall(function (target)                       end)
-                                                on_uninstall(function (target)                       end)
-
+    add_files("modulesdk.cpp")
+    on_install(function (target)end)
+    on_uninstall(function (target)end)
+    
 target("etp_module_rtmp")
     set_kind("shared")
-    add_deps("deps")
+    add_deps("deps", "etp_static_log")
     set_filename("etp_module_rtmp.so")
-    add_files("main.cpp","log.c")
+    add_files("main.cpp")
 
 
